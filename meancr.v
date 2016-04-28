61a28611db060114c68281bc88fd64e218e22ea5
gussmith23 <hfs5022@psu.edu>
2016-04-28 16:37:19 -0400

    same changes to meancr

--- meancr.v
+++ meancr.v
@@ -2,8 +2,8 @@
 
 module meancr
 (
-	input		[7:0]	Y,
-	output 			out
+	input		[7:0]					Y,
+	output 		[`meancr_width -1:0]	out
 );
 
 reg 	[`meancr_width -1:0]	memory		[0:256];
@@ -11,11 +11,9 @@ reg 	[`meancr_width -1:0]	y;
 
 assign out = y;
 
-initial begin
-	$readmemb("data/meancr_data.txt", memory);
-end
+initial $readmemb("data/meancr_data.txt", memory);
 
-always begin
+always @(Y) begin
 	y <= memory[Y];
 end
 

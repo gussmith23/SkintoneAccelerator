a8b12ca6983201b56db8eefd99dbabf8f1f8f401
gussmith23 <hfs5022@psu.edu>
2016-04-28 16:34:12 -0400

    fixes problems in meancb: missing sensitivity list, port width missing, possibly something else

--- meancb.v
+++ meancb.v
@@ -2,20 +2,18 @@
 
 module meancb
 (
-	input		[7:0]	Y,
-	output 			out
+	input		[7:0]					Y,
+	output 		[`meancb_width -1:0]	out
 );	
 
-reg 	[`meancb_width -1:0]	memory		[0:256];
+reg 	[`meancb_width -1:0]	memory		[0:255];
 reg 	[`meancb_width -1:0]	y;
 
 assign out = y;
 
-initial begin
-	$readmemb("data/meancb_data.txt", memory);
-end
+initial $readmemb("data/meancb_data.txt", memory);
 
-always begin
+always @(Y) begin
 	y <= memory[Y];
 end
 

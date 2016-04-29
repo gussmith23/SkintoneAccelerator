2a245b101a1ad985c30b6f931d0fab9e1d907d1b
gussmith23 <hfs5022@psu.edu>
2016-04-29 17:00:43 -0400

    goes back to testng main module

--- datapath_testbench.sv
+++ datapath_testbench.sv
@@ -13,7 +13,12 @@ reg				result_dataout_ready;
 
 integer cycles;
 
-/*
+class pixel_t;
+	rand bit [23:0] data;
+endclass
+
+pixel_t pixel;
+
 skintone_datapath dut(
 	clk, 
 	rst,
@@ -23,19 +28,27 @@ skintone_datapath dut(
 	result_dataout,
 	result_dataout_valid,
 	result_dataout_ready
-);*/
+);
 
 initial begin
+	pixel = new();
 	clk = 0;
 	cycles = 0;
-	//pixel_datain_valid = 0;
-	//$monitor("%d %d %d\n", cycles, pixel_datain_valid, result_dataout_valid);
+	pixel_datain_valid = 0;
+end
+
+always #10 begin
+	cycles++;
+	pixel.randomize();
+	pixel_datain_ready = 1; pixel_datain_valid = 1;
+	pixel_datain = pixel.data;
+	
+	$display("%d %d %d %d\n", cycles, pixel_datain_valid, result_dataout_valid, result_dataout);
+	
 end
 
 always #5 begin
-	//clk = !clk;
-	//cycles++;
-        $display("test");
+	clk = !clk;
 end
 
 //always #50 pixel_datain_valid = !pixel_datain_valid;

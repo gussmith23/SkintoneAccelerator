4504325906f53a279db12ce9fa792663181af528
gussmith23 <hfs5022@psu.edu>
2016-04-28 23:15:59 -0400

    finishes testing. gonna assume it's good for now, but if there're any problems, they're probably here.

--- transcr_testbench.sv
+++ transcr_testbench.sv
@@ -21,6 +21,42 @@ real found_transcr;
 
 transcr dut(clk, Cr, Y, transcr);
 
+
+function meancr_func;
+input reg [7:0] Y;
+begin
+if (Y <= `K_l )
+  meancr_func = 154.0 + 10.0*(`K_l - Y)/(`K_l - `Y_min);
+else if (`K_h <= Y)
+  meancr_func = 154.0 + 22.0*(Y - `K_h)/(`Y_max - `K_h);
+else meancr_func = 0.0;
+end
+endfunction
+
+
+function widthcr_func;
+input reg [7:0] Y;
+begin
+if (Y <= `K_l )
+  widthcr_func = `WL_Cr + (Y - `Y_min)*(`W_Cr - `WL_Cr)/(`K_l - `Y_min);
+else if (`K_h <= Y)
+  widthcr_func = `WH_Cr + (`Y_max - Y)*(`W_Cr - `WH_Cr)/(`Y_max - `K_h);
+else widthcr_func = 0.0;
+end
+endfunction
+
+function transcr_func;
+input reg [7:0] Y;
+input reg [7:0] Cr;
+begin
+if (`K_l <= Y && Y <= `K_h)
+  transcr_func = $itor(Cr);
+else
+  transcr_func = (Cr - meancr_func(Y)) * (`W_Cr / widthcr_func(Y)) + meancr_func(`K_h);
+end
+endfunction
+
+
 initial begin
 	$monitor("%b\t%b\t%f\t%f", Cr, Y, found_transcr, actual_transcr);
 	clk = 0;
@@ -33,7 +69,9 @@ always #5 begin
 	Cr = pair.pair[15:8];
 	Y = pair.pair[7:0];
 	
-		
+        actual_transcr = transcr_func(Y, Cr);
+
+	/*	
 	// Calculate meancr_y
 	if(Y <= `K_l )
 		meancr_y = 154.0 + 10.0*(`K_l - Y)/(`K_l - `Y_min );
@@ -63,7 +101,7 @@ always #5 begin
 		actual_transcr =  Cr;
 	else
 		actual_transcr = (Cr - meancr_y) * (`W_Cr / widthcr_y) + meancr_kh;
-		
+	*/	
 end
 
 always @ (transcr) begin

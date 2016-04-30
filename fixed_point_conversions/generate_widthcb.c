6280b67cb70f4eaa782505d57309dc82d7dba8b9
Gus Smith <gus@justg.us>
2016-04-30 17:35:56 -0400

    realized that widthcb/cr should actually be w_cr/widthcr and w_cb/widthcb. i think i remembered this a while ago and never fixed it...

--- fixed_point_conversions/generate_widthcb.c
+++ fixed_point_conversions/generate_widthcb.c
@@ -7,13 +7,14 @@
 #define DEBUG 0
 
 // TODO error increases significantly at the end.
+// NOTE: Like the other file, this code isn't generating WidthCb but W_Cb/WidthCb. 
 
 int main()
 {
 	for (int i = 0; i < 256; i++)
 	{
 		double Y = i;
-		double output = WidthCb(Y);
+		double output = W_Cb / WidthCb(Y);
 		
 		if (DEBUG)	
 			std::cout << "Should be: " << output << " is: " << convert_from_fixed_point(convert_to_fixed_point(output)) << std::endl;
--- fixed_point_conversions/generate_widthcr.c
+++ fixed_point_conversions/generate_widthcr.c
@@ -6,12 +6,14 @@
 
 #define DEBUG 0
 
+// NOTE: This code isn't generating WidthCr but W_Cr/WidthCr. Bad filename, I know...
+
 int main()
 {
 	for (int i = 0; i < 256; i++)
 	{
 		double Y = i;
-		double output = WidthCr(Y);
+		double output = W_Cr / WidthCr(Y);
 		
 		if (DEBUG)	
 			std::cout << "Should be: " << output << " is: " << convert_from_fixed_point(convert_to_fixed_point(output)) << std::endl;

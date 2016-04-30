bddabfbbe0144b82ad0781d03ce994343150631c
Gus Smith <gus@justg.us>
2016-04-30 10:45:01 -0400

    adds comment to mult testbench about a minor bug

--- fp_mult_testbench.sv
+++ fp_mult_testbench.sv
@@ -1,5 +1,11 @@
 `include "datapath.vh"
 
+// TODO this module and add_sub_testbench display misleading results;
+// they say something like a1 * b1 ~= c1 \n a2 * b2 ~= c2, though
+// a1 * b1's result is actually c2. so everything is shifted down, in a way.
+// However, the modules both work correctly, so correcting this
+// isn't a high priority.
+
 module fp_mult_testbench();
 
 reg signed		[`fp_width-1:0]		a, b, out;
